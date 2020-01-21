4a4e040b52b3393caf0d66e1145c6d4f99ed8777
John O'Sullivan <john@eximchain.com>
2020-01-21 11:50:01 -0500

    Added readouts for code size

--- src/ui/DeployDetails/Overview.tsx
+++ src/ui/DeployDetails/Overview.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Color } from 'ink';
 import moment from 'moment';
+import prettyBytes from 'pretty-bytes';
 import { DeployStates, DeployItem } from "@eximchain/ipfs-ens-types/spec/deployment";
 import { Rows, Column, ChevronText, Row } from '../helpers';
 import { DetailHeader } from '../DeployDetails';
@@ -11,10 +12,6 @@ export const Overview: FC<DeployItem> = (props) => {
   const { ensName, owner, repo, createdAt, state, branch, transitionError, transitions } = props;
   const createMoment = moment(createdAt);
   const now = moment();
-
-  // TODO: If we have a failure, or if we have the last transition,
-  // add a row saying what happened.
-  let completionRow = 'null';
 
   let stateMsg = transitionError ? `Error: ${state}` : state;
   // Dark Yellow / Gold
@@ -40,9 +37,34 @@ export const Overview: FC<DeployItem> = (props) => {
       {`Deployment failed during the ${state} stage`}
     </ChevronText>
   )
+  if (transitions.source) OverviewRows.push(
+    <ChevronText key='source-size'>
+      {`Source code size was ${prettyBytes(transitions.source.size)}`}
+    </ChevronText>
+  )
+  if (transitions.build) OverviewRows.push(
+    <ChevronText key='build-size'>
+      {`Build size was ${prettyBytes(transitions.build.size)}`}
+    </ChevronText>
+  )
+  if (transitions.ipfs) OverviewRows.push(
+    <ChevronText key='ipfs-hash'>
+      {`IPFS Upload hash was ${transitions.ipfs.hash}`}
+    </ChevronText>
+  )
+  if (transitions.ensRegister) OverviewRows.push(
+    <ChevronText key=''>
+      {`Registered your subdomain with transaction ${transitions.ensRegister.txHash}`}
+    </ChevronText>
+  )
+  if (transitions.ensSetResolver) OverviewRows.push(
+    <ChevronText key=''>
+      {`Added a resolver with transaction ${transitions.ensSetContent?.txHash}`}
+    </ChevronText>
+  )
   if (transitions.ensSetContent) OverviewRows.push(
     <ChevronText key='complete'>
-      {`Deployment completed ${timeStr(createdAt, transitions.ensSetContent.timestamp)}`}
+      {`Deployment completed ${timeStr(createdAt, transitions.ensSetContent.timestamp)}, final transaction hash was ${transitions.ensSetContent.txHash}`}
     </ChevronText>
   )
   return (
